1c8f87e45f7307581c532d2ff40a9d1d6dc812c1
M K <mkpro118@gmail.com>
2024-06-08 17:15:33 -0500

    Implemented `next_token`

--- src/lexer.c
+++ src/lexer.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <portability.h>
+#include <ctype.h>
 
 #include "lexer.h"
 
@@ -27,6 +28,58 @@ int lexer_init(Lexer* lexer, char* regex) {
 
     lexer->_regex = cpy;
     lexer->_position = 0;
+    lexer->_regex_lex = strlen(cpy);
+
+    return 0;
+}
+
+// Finds the next token with the given lexer state
+// Result is filled in the token parameter
+int next_token(Lexer* lexer, Token* token) {
+    token->value = 0; // Reset token value regardless
+
+    if (lexer->_position >= lexer->_regex_lex) {
+        token->type = EOF;
+        return 0;
+    }
+
+    char character = lexer->_regex[lexer->_position++];
+
+    switch (character) {
+    case '(':
+        token->type = LPAREN;
+        break;
+
+    case '|':
+        token->type = OR;
+        break;
+
+    case '+':
+        token->type = PLUS;
+        break;
+
+    case '?':
+        token->type = QUESTION;
+        break;
+
+    case ')':
+        token->type = RPAREN;
+        break;
+
+    case '*':
+        token->type = STAR;
+        break;
+
+    default:
+        // We do not support non-printable characters
+        if (!isprint(character)) {
+            token->type = ERROR;
+            return -1;
+        }
+
+        token->type = CHAR;
+        token->value = character;
+    }
 
     return 0;
 }
