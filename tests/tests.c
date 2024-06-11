4746b4c9ed2adafedfc9d1a5f32a676541f3804d
M K <mkpro118@gmail.com>
2024-06-11 17:29:17 -0500

    improved help message

--- tests/tests.c
+++ tests/tests.c
@@ -7,7 +7,9 @@ struct TestSuite {
     TestOpts* opts;
 };
 
-const char tests_help_str[] = "Available options are:\n"
+const char tests_help_str[] =
+"Parse args to create a set of options used by the test runner.\n"
+"Available options are:\n"
 "  -ff, --fail-fast: Stop running tests after the first failure.\n"
 "  -v, --verbose <level>: Runner's versbosity level. 0, 1 or 2\n"
 "  -r, --run <test_name>: Run only the specified test(s).\n"
@@ -15,7 +17,8 @@ const char tests_help_str[] = "Available options are:\n"
 "  -o, --output <file>: Redirect test results to a specified file.\n"
 "  -t, --timeout <milliseconds>: Set a maximum runtime for each test.\n"
 "  -d, --dry-run: Display the tests that would be run without executing them.\n"
-"  -s, --summary: Print only the summary of the test results. (Sets verbosity to 0)\n"
+"  -s, --summary: Print only the summary of the test results. (Sets verbosity "
+"to 0)\n"
 "  -z, --randomize: Run tests in a random order.\n";
 
 // A help/usage string for the options used by a Test Suite
