f1dc8d96ea9865d2b847025b3e6d3c799cf50053
M K <mkpro118@gmail.com>
2024-06-12 18:49:52 -0500

    Fixed macro variable expansions and arguments

--- tests/testlib/asserts.h
+++ tests/testlib/asserts.h
@@ -35,7 +35,7 @@ extern int ASSERTS_EXIT_CODE;
 /* Compare integers, and display a message about the failure */
 #define assert_equals_int(a, b) do {\
     if ((a) != (b)) {\
-        ASSERTION_FAILED("%d != %d\n", (a), (b));\
+        ASSERTION_FAILED("%ld != %ld\n", (long int)(a), (long int)(b));\
     }\
 } while(0)
 
@@ -48,11 +48,13 @@ extern int ASSERTS_EXIT_CODE;
 } while(0)
 
 
-static int compar_int(const void* a, const void* b) {
+// Comparator function for ints
+static inline int compar_int(const void* a, const void* b) {
     return *((const int*) a) - *((const int*) b);
 }
 
-static int compar_str(const void* a, const void* b) {
+// Type safe wrapper over strcmp, comparator for strings
+static inline int compar_str(const void* a, const void* b) {
     return strcmp((const char*) a, (const char*) b);
 }
 
@@ -79,14 +81,14 @@ static int compar_str(const void* a, const void* b) {
     \
 } while(0)
 
-#define _test_arr_sort_(x, y, type, comparator) \
+#define _test_arr_sort_(x, y, sz, type, comparator) \
 /* Allocate memory to copy array, we do not modify original array */\
-type* _arr1 = malloc(sizeof(type) * (size));\
+type* _arr1 = malloc(sizeof(type) * (sz));\
 if (_arr1 == NULL) { /* Verify malloc worked */ \
     fprintf(stderr, "Allocator failed. strerror: %s\n", strerror(errno));\
     exit(EXIT_FAILURE);\
 }\
-type* _arr2 = malloc(sizeof(type) * (size));\
+type* _arr2 = malloc(sizeof(type) * (sz));\
 if (_arr2 == NULL) { /* Verify malloc worked */ \
     free(_arr1);\
     fprintf(stderr, "Allocator failed. strerror: %s\n", strerror(errno));\
@@ -94,20 +96,20 @@ if (_arr2 == NULL) { /* Verify malloc worked */ \
 }\
 \
 /* Copy array to sort, we do not modify original array */\
-memcpy(_arr1, x, sizeof(type) * (size));\
-memcpy(_arr2, y, sizeof(type) * (size));\
-qsort(_arr1, (size), sizeof(type), comparator);\
-qsort(_arr2, (size), sizeof(type), comparator)
+memcpy(_arr1, x, sizeof(type) * (sz));\
+memcpy(_arr2, y, sizeof(type) * (sz));\
+qsort(_arr1, (sz), sizeof(type), comparator);\
+qsort(_arr2, (sz), sizeof(type), comparator)
 
 /* Compare arrays of integers (unordered), and display a message about the failure */
 #define assert_equals_int_array_unordered(a, b, size) do {\
     mem_equals((a), (b), int*);\
-    _test_arr_sort_((a), (b), int, compar_int);\
+    _test_arr_sort_((a), (b), (size), int, compar_int);\
     for (int i = 0; i < (size); i++) {\
         if ((a)[i] != (b)[i]) {\
             free(_arr1);\
             free(_arr2);\
-            ASSERTION_FAILED("Mismatched elements, %d in " #a " not found in " #b ".\n", (a)[i]);\
+            ASSERTION_FAILED("Mismatched elements (index %d after sorting asc.), %d in " #a " not found in " #b ".\n", i, (a)[i]);\
         }\
     }\
     free(_arr1);\
@@ -117,12 +119,12 @@ qsort(_arr2, (size), sizeof(type), comparator)
 /* Compare arrays of strings (unordered), and display a message about the failure */
 #define assert_equals_str_array_unordered(a, b, size) do {\
     mem_equals((a), (b), int*);\
-    _test_arr_sort_((a), (b), char*, compar_str);\
+    _test_arr_sort_((a), (b), (size), char*, compar_str);\
     for (int i = 0; i < (size); i++) {\
         if (strcmp((a)[i], (b)[i]) != 0) {\
             free(_arr1);\
             free(_arr2);\
-            ASSERTION_FAILED("Mismatched elements, %s in " #a " not found in " #b ".\n",(a)[i]);\
+            ASSERTION_FAILED("Mismatched elements (index %d after sorting with strcmp), %s in " #a " not found in " #b ".\n", i, (a)[i]);\
         }\
     }\
     free(_arr1);\
