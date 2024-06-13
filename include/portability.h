17e8b3426289ca8a547925f9033d0d47bb80dafd
M K <mkpro118@gmail.com>
2024-06-13 16:47:50 -0500

    added `n_processors_online` to determine max number of threads

--- include/portability.h
+++ include/portability.h
@@ -7,14 +7,24 @@
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
    //define something for Windows (32-bit and 64-bit, this part is common)
 
-char* strdup(const char* source);
+    #define WIN32_LEAN_AND_MEAN
+    #include <windows.h>
+    char* strdup(const char* source);
 
 #elif __unix__ // all unices not caught above
-    // Unix
+    #include <unistd.h>
 #elif defined(_POSIX_VERSION)
-    // POSIX
+    #include <unistd.h>
 #else
 #error "Unknown compiler"
 #endif
 
 #endif // _REGEX_PORTABILITY_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+
+int n_processors_online();
