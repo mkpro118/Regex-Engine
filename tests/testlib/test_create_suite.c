114678fb967db9919faead4c7e800c7a49aa40f7
M K <mkpro118@gmail.com>
2024-06-16 01:18:24 -0500

    Tests to check the correct functions are in the suite

--- tests/testlib/test_create_suite.c
+++ tests/testlib/test_create_suite.c
@@ -4,31 +4,75 @@
 #include "tests.h"
 #include "asserts.h"
 
+// Dummy functions to tests
+void test_everything(void){}
+void unit_test_1(void){}
+void unit_test_2(void){}
+void fail_unit_test_1(void){}
+void fail_unit_test_2(void){}
+void unit_fail_test_1(void){}
+
 Test tests[] = {
-    {.name="test_everything", .func=NULL},
-    {.name="unit_test_1", .func=NULL},
-    {.name="unit_test_2", .func=NULL},
-    {.name="fail_unit_test_1", .func=NULL},
-    {.name="fail_unit_test_1", .func=NULL},
-    {.name="unit_fail_test_1", .func=NULL},
+    {.name="test_everything", .func=test_everything},
+    {.name="unit_test_1", .func=unit_test_1},
+    {.name="unit_test_2", .func=unit_test_2},
+    {.name="fail_unit_test_1", .func=fail_unit_test_1},
+    {.name="fail_unit_test_2", .func=fail_unit_test_2},
+    {.name="unit_fail_test_1", .func=unit_fail_test_1},
     {.name=NULL},
 };
+
+// -1 to exclude the null
+const size_t tests_size = sizeof(tests) / sizeof(Test) - 1;
+
+// Sort suite tests relatively to global variable tests
+int test_sort_compar(const void* a, const void* b) {
+    const Test* ta = *((const Test**) a);
+    const Test* tb = *((const Test**) b);
+
+    int idxa = -1;
+    int idxb = -1;
+    for (size_t i = 0; i < tests_size; i++) {
+        if (strcmp(tests[i].name, ta->name) == 0) {idxa=i;}
+        if (strcmp(tests[i].name, tb->name) == 0) {idxb=i;}
+    }
+
+    if (idxa < 0) {
+        fprintf(stderr, "TEST ERROR: (A) Didn't find %s %p\n", ta->name, (void*)ta);
+        exit(1);
+    }
+    if (idxb < 0) {
+        fprintf(stderr, "TEST ERROR: (B) Didn't find %s\n", tb->name);
+        exit(1);
+    }
+
+    if (idxa == idxb) {
+        fprintf(stderr, "TEST ERROR: SORT ERROR!!! (A, B)===(%d, %d)\n", idxa, idxb);
+        exit(1);
+    }
+
+    return idxa - idxb;
+}
 
 TestSuite const* suite;
 TestOpts opts;
 
+// Determines how options are configured and resource release strategy
 enum OptionStrategy {
     AUTOMATIC,
     MANUAL,
 } opt_strategy = AUTOMATIC;
 
+
+// Initialize options
 void reset_opts() {
     opt_strategy = AUTOMATIC;
     opts = (TestOpts) {0};
 
-    // -1 to exclude the null
-    opts.included_size = (sizeof(tests) / sizeof(Test)) - 1;
+    opts.included_size = tests_size;
     opts.included = malloc(sizeof(char*) * opts.included_size);
+
+    // Sanity checks
     assert_is_not_null(opts.included);
     assert_is_null(opts.excluded);
     assert_is_null(opts.output_file);
@@ -38,6 +82,7 @@ void reset_opts() {
     }
 }
 
+// Release resources
 void after_each() {
     free_test_suite(suite);
 
@@ -86,7 +131,7 @@ void test_create_suite_ok_2(void) {
     suite = create_test_suite(&opts);
     assert_is_not_null(suite);
 
-    assert_equals_int(suite->n_tests, 4);
+    assert_equals_int(suite->n_tests, opts.included_size);
     assert_is_not_null(suite->tests);
 
     int size = (int)suite->n_tests;
@@ -112,7 +157,7 @@ void test_create_suite_ok_3(void) {
     suite = create_test_suite(&opts);
     assert_is_not_null(suite);
 
-    assert_equals_int(suite->n_tests, 2);
+    assert_equals_int(suite->n_tests, opts.included_size);
     assert_is_not_null(suite->tests);
 
     char** included = &options[1];
@@ -127,11 +172,29 @@ void test_create_suite_ok_3(void) {
     assert_equals_str_array_unordered(actual, included, size);
 }
 
+// Ensure the test functions are correct
+void test_create_suite_ok_4(void) {
+    reset_opts();
+    suite = create_test_suite(&opts);
+    assert_is_not_null(suite);
+    assert_is_not_null(suite->tests);
+    assert_equals_int(suite->n_tests, tests_size);
+
+    // Order for comparison
+    qsort(suite->tests, tests_size, sizeof(Test*), test_sort_compar);
+
+    for (size_t i = 0; i < tests_size; i++) {
+        assert_equals_str(suite->tests[i]->name, tests[i].name);
+        assert_equals_funcptr(suite->tests[i]->func, tests[i].func, test_func_t);
+    }
+}
+
 Test curr_tests[] = {
     {.name="test_create_suite_ok_0", .func=test_create_suite_ok_0},
     {.name="test_create_suite_ok_1", .func=test_create_suite_ok_1},
     {.name="test_create_suite_ok_2", .func=test_create_suite_ok_2},
     {.name="test_create_suite_ok_3", .func=test_create_suite_ok_3},
+    {.name="test_create_suite_ok_4", .func=test_create_suite_ok_4},
 };
 
 int main(void) {
