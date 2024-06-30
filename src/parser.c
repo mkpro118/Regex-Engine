634680d671d8ac33bce86cfbdcdeb3ea2c055b85
M K <mkpro118@gmail.com>
2024-06-30 17:31:32 -0500

    Implemented next

--- src/parser.c
+++ src/parser.c
@@ -24,7 +24,6 @@ Token* peek(Parser* parser){
 
 
 /**
- * TODO: WIP
  * Returns the next token and advances the parser's position
  *
  * @param parser The parser to get the next token from
@@ -32,7 +31,15 @@ Token* peek(Parser* parser){
  * @return A pointer to the next token if it exists, NULL otherwise
  */
 Token* next(Parser* parser){
-    return parser != NULL ? NULL : NULL;
+    if (parser == NULL) {
+        return NULL;
+    }
+
+    if (parser->position >= parser->n_tokens) {
+        return NULL;
+    }
+
+    return &(parser->tokens[parser->position++]);
 }
 
 
