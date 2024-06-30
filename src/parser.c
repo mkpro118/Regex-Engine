3d379f94e738cae3cafe89350602efedfb7a2a62
M K <mkpro118@gmail.com>
2024-06-30 02:03:35 -0500

    Let the functions compile, move todo tags to header comments

--- src/parser.c
+++ src/parser.c
@@ -4,42 +4,40 @@
 #include "token.h"
 
 /**
+ * TODO: WIP
  * Returns the next token with advancing the parser's position
  *
  * @param  parser The parser to peek with
  *
  * @return A pointer to the next token if it exists, NULL otherwise
  */
-Token* peek(Parser* parser) {
-    // TODO
-}
+Token* peek(Parser* parser);
 
 
 /**
+ * TODO: WIP
  * Returns the next token and advances the parser's position
  *
  * @param parser The parser to get the next token from
  *
  * @return A pointer to the next token if it exists, NULL otherwise
  */
-Token* next(Parser* parser) {
-    // TODO
-}
+Token* next(Parser* parser);
 
 
 /**
+ * TODO: WIP
  * Ensure that the next token is of a given type
  *
  * @param  parser The parser to check with
  *
  * @return 0 if the next token is of the required type, -1 otherwise
  */
-int expect(Parser* parser, ASTNodeType type) {
-    // TODO
-}
+int expect(Parser* parser, ASTNodeType type);
 
 
 /**
+ * TODO: WIP
  * Parse the `base` non-terminal.
  *
  * See the regex CFG on
@@ -52,12 +50,11 @@ int expect(Parser* parser, ASTNodeType type) {
  *
  * @return The root of the AST created by parsing the `base` non-terminal
  */
-ASTNode* parse_base(Parser* parser) {
-    // TODO
-}
+ASTNode* parse_base(Parser* parser);
 
 
 /**
+ * TODO: WIP
  * Parse the `factor` non-terminal.
  *
  * See the regex CFG on
@@ -70,12 +67,11 @@ ASTNode* parse_base(Parser* parser) {
  *
  * @return The root of the AST created by parsing the `factor` non-terminal
  */
-ASTNode* parse_factor(Parser* parser) {
-    // TODO
-}
+ASTNode* parse_factor(Parser* parser);
 
 
 /**
+ * TODO: WIP
  * Parse the `term` non-terminal.
  *
  * See the regex CFG on
@@ -88,12 +84,11 @@ ASTNode* parse_factor(Parser* parser) {
  *
  * @return The root of the AST created by parsing the `term` non-terminal
  */
-ASTNode* parse_term(Parser* parser) {
-    // TODO
-}
+ASTNode* parse_term(Parser* parser);
 
 
 /**
+ * TODO: WIP
  * Parse the `expr` non-terminal.
  *
  * See the regex CFG on
@@ -106,6 +101,4 @@ ASTNode* parse_term(Parser* parser) {
  *
  * @return The root of the AST created by parsing the `expr` non-terminal
  */
-ASTNode* parse_expr(Parser* parser) {
-    // TODO
-}
+ASTNode* parse_expr(Parser* parser);
