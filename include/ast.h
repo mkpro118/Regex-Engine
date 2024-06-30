d52d5cd0ec96fccb03b6045fee85bc84f7322efe
M K <mkpro118@gmail.com>
2024-06-29 23:04:19 -0500

    Defined enum `ASTNodeTypes`

---
+++ include/ast.h
@@ -0,0 +1,11 @@
+/**
+ * Lists the types of AST Nodes
+ */
+typedef enum ASTNodeType {
+    CHAR_NODE,
+    STAR_NODE,
+    PLUS_NODE,
+    QUESTION_NODE,
+    OR_NODE,
+    CONCAT_NODE,
+} ASTNodeType;
