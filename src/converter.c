7f3c2eb53e47bb18c154fdb6a757e4fa9b8d2c7f
M K <mkpro118@gmail.com>
2024-07-03 13:43:50 -0500

    Skeleton converter

--- src/converter.c
+++ src/converter.c
@@ -4,4 +4,28 @@
 #include "converter.h"
 
 // Convert AST to NFA
-NFA* convert_ast_to_nfa(ASTNode* node);
+NFA* convert_ast_to_nfa(ASTNode* root) {
+    switch (root->type) {
+    case CHAR_NODE:
+        break;
+
+    case STAR_NODE:
+        break;
+
+    case PLUS_NODE:
+        break;
+
+    case QUESTION_NODE:
+        break;
+
+    case OR_NODE:
+        break;
+
+    case CONCAT_NODE:
+        break;
+
+    default:
+        // Ideally should never get here
+        return NULL;
+    }
+}
