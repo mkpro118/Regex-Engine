902ea431d3c86c22b652330e02d92fbeb862141d
M K <mkpro118@gmail.com>
2024-07-03 13:46:00 -0500

    added documentation

--- include/converter.h
+++ include/converter.h
@@ -4,6 +4,15 @@
 #include "nfa.h"
 #include "ast.h"
 
+/**
+ * Convert an AST rooted at root to a NFA
+ * AST = Abstract Syntax Tree
+ * NFA = Non-deterministic Finite Automata
+ *
+ * @param  root The root of the abstract syntax tree
+ * 
+ * @return An NFA equivalent to the given AST
+ */
 NFA* convert_ast_to_nfa(ASTNode* root);
 
 #endif // REGEX_CONVERTER_H
