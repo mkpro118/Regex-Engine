9c3b3f990eefc8d1ea1a62b45d4b6acbd21ad8a6
M K <mkpro118@gmail.com>
2024-07-03 20:02:30 -0500

    Fail Fast

--- tests/test_regex.c
+++ tests/test_regex.c
@@ -1,11 +1,19 @@
+#define FAIL_FAST
 #include "testlib/asserts.h"
 #include "testlib/tests.h"
 
 #include "regex.h"
 
+int test_regex_easy(void) {
+    TEST_BEGIN;
 
+
+
+    TEST_END;
+}
 
 Test tests[] = {
+    {.name="test_regex_easy", .func=test_regex_easy},
     {.name=NULL, .func=NULL},
 };
 
